a835468a0b1dcb2321cf3ae6a73f40031741b222
mac <hackxboy1986@163.com>
2023-03-27 15:48:47 +0800

    c

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,6 @@ from .views import Comixify, ComixifyImg,ComixifyFromYoutube
 
 urlpatterns = [
     path(r'', Comixify.as_view(), name='comixify'),
-    path(r'', ComixifyImg.as_view(), name='comixifyImg'),
+    path(r'img/', ComixifyImg.as_view(), name='comixifyImg'),
     path(r'from_yt/', ComixifyFromYoutube.as_view(), name='comixify_from_yt'),
 ]
