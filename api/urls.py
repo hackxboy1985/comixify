162bd687c91e41ef31c07515142623727d60f965
mac <hackxboy1986@163.com>
2023-03-27 15:15:48 +0800

    Url

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import Comixify, ComixifyFromYoutube
+from .views import Comixify, ComixifyImg,ComixifyFromYoutube
 
 urlpatterns = [
     path(r'', Comixify.as_view(), name='comixify'),
+    path(r'', ComixifyImg.as_view(), name='comixifyImg'),
     path(r'from_yt/', ComixifyFromYoutube.as_view(), name='comixify_from_yt'),
 ]
